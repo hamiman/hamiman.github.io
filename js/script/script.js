40999a570d3eb95423bfeb304406e38237dc47d3
hamiman <hamiman.mim@gmail.com>
2017-01-29 16:47:40 +0700

    add fix navbar, about, portfolio, contact, image header, imagecontact, scroll animation.

--- js/script/script.js
+++ js/script/script.js
@@ -1,7 +1,31 @@
-	// burger menu
+	
 	$(document).ready(function(){
+		// burger menu
 		$("#burger-container").on("click",function(){
-			$("#burger-container").toggleClass("open");
+			$(this).toggleClass("open");
 			$("#menu").toggleClass("open");
 		});
+		// fixed navbar
+		var navOffset = $("nav").offset().top;
+
+		$(window).scroll(function(){
+			var scrollFix = $(window).scrollTop();
+			if (scrollFix >= navOffset){
+				$("nav").addClass("navbar-fixed-top");
+			}else {
+				$("nav").removeClass("navbar-fixed-top");
+			}
+		});
+		// page-scroll
+		$(".page-scroll").on("click", function(){
+			var href = $(this).attr("href");
+			var elementHref = $(href);
+
+			$("body").animate({
+				scrollTop: elementHref.offset().top - 50
+			},1500,"easeInOutExpo");
+		});
+		// *****
 	});
+	
+
